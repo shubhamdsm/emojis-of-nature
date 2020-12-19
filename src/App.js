7b287537d7f4f0f3986df98ca76626d4aab63cf9
shubham <33717931+shubhamdsm@users.noreply.github.com>
2020-12-20 03:46:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojisOfNature = {
+  "☀": "sunny",
+  "☂": "umbrella",
+  "☁": "cloud",
+  "❄": "snowflake",
+  "⛄": "snowman",
+  "⚡": "zap",
+  "🌀": "cyclone",
+  "🌁": "foggy",
+  "🐄": "cow",
+  "🌹": "rose"
+};
+
+var emojis = Object.keys(emojisOfNature);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function onChangeHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojisOfNature[userInput];
+    setMeaning(meaning);
+  }
+
+  function onClickHandler(emoji) {
+    var meaning = emojisOfNature[emoji];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1>Emojis of Nature</h1>
+      <input onChange={onChangeHandler} />
+      <h2>{meaning}</h2>
+      <h3>List of Emojis</h3>
+      {emojis.map(function (emoji) {
+        return (
+          <span
+            onClick={() => onClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
